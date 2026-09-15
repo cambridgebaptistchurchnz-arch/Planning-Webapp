71803bbcbb797e2d8e99fbaad836690cbab6e21f
cambridgebaptistchurchnz-arch <cambridgebaptistchurchnz@gmail.com>
2026-09-16 11:06:53 +1200

    Send emails via Resend API instead of SMTP (Render blocks SMTP on free tier)

--- roster/settings.py
+++ roster/settings.py
@@ -137,17 +137,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # password, etc. During local dev with no EMAIL_HOST set, emails just
 # print to the console instead of actually sending.
 
-if os.environ.get("EMAIL_HOST"):
-    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-    EMAIL_HOST = os.environ.get("EMAIL_HOST")
-    EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587"))
-    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-    EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True") == "True"
-else:
-    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
-DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "roster@example.org")
+RESEND_API_KEY = os.environ.get("RESEND_API_KEY")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "onboarding@resend.dev")
 
 # Public base URL used to build the links volunteers click in emails.
 # e.g. https://church-roster.onrender.com
