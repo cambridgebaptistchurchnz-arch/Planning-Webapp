525749e3c2477ecf36d6365f9e3bbc6d6bb0112a
cambridgebaptistchurchnz-arch <cambridgebaptistchurchnz@gmail.com>
2026-09-16 10:53:15 +1200

    Fix superuser creation command

---
+++ scheduling/management/commands/create_default_superuser.py
@@ -0,0 +1,26 @@
+import os
+
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Creates a superuser from ADMIN_USERNAME / ADMIN_EMAIL / ADMIN_PASSWORD env vars."
+
+    def handle(self, *args, **options):
+        username = os.environ.get("ADMIN_USERNAME")
+        email = os.environ.get("ADMIN_EMAIL", "")
+        password = os.environ.get("ADMIN_PASSWORD")
+
+        if not username or not password:
+            self.stdout.write("ADMIN_USERNAME / ADMIN_PASSWORD not set -- skipping.")
+            return
+
+        User = get_user_model()
+
+        if User.objects.filter(username=username).exists():
+            self.stdout.write(f"Superuser '{username}' already exists -- skipping.")
+            return
+
+        User.objects.create_superuser(username=username, email=email, password=password)
+        self.stdout.write(self.style.SUCCESS(f"Created superuser '{username}'."))
